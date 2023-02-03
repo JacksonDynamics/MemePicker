1b71e0edbe89ab3f7e098ca013668b95edb17ce1
studmanj <bjackso3@hotmail.com>
2023-02-02 22:26:28 -0700

    highlight radio parent

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import {catsData} from "/data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-emotionRadios.addEventListener('change', (e)=> {
-    console.log(e.target.id)
-})
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radioClasses = document.getElementsByClassName('radio')
+    
+    for (let radio of radioClasses){
+        radio.classList.remove('highlight')
+    }
+    
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []
