62bca54c5158fb2ea4acfc326c91e4c97a01084f
studmanj <bjackso3@hotmail.com>
2023-02-02 21:41:18 -0700

    e.target.id

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import {catsData} from "/data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', (e)=> {
+    console.log(e.target.id)
+})
 
 function getEmotionsArray(cats){
     const emotionsArray = []
