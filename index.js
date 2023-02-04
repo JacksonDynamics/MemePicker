065c0cacf17c030a98906067d594cfaf18737c03
studmanj <bjackso3@hotmail.com>
2023-02-03 18:17:01 -0700

    querySelector

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import {catsData} from "/data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImage = document.getElementById('get-image-btn')
+
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
@@ -10,9 +12,18 @@ function highlightCheckedOption(e){
     for (let radio of radioClasses){
         radio.classList.remove('highlight')
     }
-    
+
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
+
+
+getImage.addEventListener("click", getMatchingCatsArray)
+
+function getMatchingCatsArray(){
+    const checkedRadio = document.querySelector("input[type='radio']:checked")
+    console.log(checkedRadio.value)
+}
+
 
 function getEmotionsArray(cats){
     const emotionsArray = []
